188b60b85f86e7de303958a0e03b968d0f56a45f
RafaelLoisW-2125250092 <rafaelois17@mhs.mdp.ac.id>
2023-03-01 08:51:54 +0700

    CobaLogin

--- app/src/main/java/com/example/cobalogin/MainActivity.java
+++ app/src/main/java/com/example/cobalogin/MainActivity.java
@@ -2,15 +2,37 @@ package com.example.cobalogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private TextView tvSesi;
+    private Button btnLogout;
 
+    KendaliLogin KL = new KendaliLogin();
+
+    public static String keySPUsername = "spPadang";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        if(KL.isLogin(MainActivity.this, keySPUsername) == true)
+        {
+            setContentView(R.layout.activity_main);
+
+            tvSesi = findViewById(R.id.tv_sesi);
+            btnLogout = findViewById(R.id.btn_logout);
+        }
+       else{
+           startActivity(new Intent(MainActivity.this, LoginActivity.class));
+           finish();
+        }
+
+
     }
 }
